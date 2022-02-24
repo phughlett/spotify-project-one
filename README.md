5779db33e2f7ea30eaafbc4e1cd1b8d4f1c45208
Patrick Hughlett <phughlett87@gmail.com>
2022-02-24 15:58:11 -0700

    End of Day 1, Login Button, can search, LogoutButton

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![alt text](image.jpg)
+
 # Reactify
 *Spotify, but on React.js*
 
@@ -17,6 +19,8 @@ Reactify is a React application that explores the functionality of the Spotify A
 This project is an intro into exploring web API's and how to utilize them within a React application. The project utilizes functional React components, React Hooks, and React Routers to develop a simple web application with the Spotify API. The web application allows a user, after logging into their Spotify account, to view their Playlists, Artists, and Albums, and also search for new music or a random song and add it to their library. In addition, users are able to listen to the music from the web applicaiton in their browser.
 
 ## Installation
+
+`code`
 
 ## Usage
 
--- src/App.js
+++ src/App.js
@@ -1,24 +1,143 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import AppContext from './context/AppContext';
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import SearchAppBar from './components/AppBar'
+
+
 
 function App() {
+
+  //states
+  const [token, setToken] = useState( "" )
+  const [searchKey, setSearchKey] = useState( "" )
+  const [artists, setArtists] = useState( [] )
+
+
+
+  //Spotify API login information and Constants
+  const CLIENT_ID = "ac38f5f06516492a855d792e3a73b558" //protect this with your life
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+  const SEARCH_URL = "https://api.spotify.com/v1/search"
+
+  //https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10
+
+  //helper functions
+
+  const search = async (e) => {
+    //e.preventDefault()
+
+    const {data} = await axios.get(SEARCH_URL, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        q: searchKey,
+        type: "artist"
+      }
+    })
+    console.log("DATA: ", data)
+    setArtists(data.artists.items)
+  }
+
+  // const search = async (e) => {
+  //   const headers = {
+  //     'Accept': 'application/json',
+  //     'Content-Type': 'application/json',
+  //     'Authorization': 'Bearer ' + token
+  //   }
+  //   const params = {
+  //     q: searchKey,
+  //     type: "artist",
+  //     limit: 10
+  //   }
+
+    //this should work maybe
+//   fetch(`https://api.spotify.com/v1/search?${fetchURL}&type=track`, {
+//     method: "GET",
+//     headers: {
+//       Authorization: `Bearer ${token}`
+//     },
+//     params: {
+//       q: searchKey,
+//       type: "artist"
+//     }
+//   }
+// )
+  //   fetch(SEARCH_URL, { method: 'GET', headers, params })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log("DATA: ", data)
+  //       setArtists(data.artists.items)
+  //     })
+  // }
+
+  const renderTrack = () => {
+    return artists.map((artist) => (
+      <div key={artist.id}>
+        {artist.images.length ? <img width={"50%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
+        {artist.name}
+      </div>
+    ))
+  }
+
+  useEffect(() => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    if (!token && hash) {
+      token = hash.substring(1)
+                  .split("&")
+                  .find(elem => elem.startsWith("access_token"))
+                  .split("=")[1]
+      window.location.hash = ""
+      window.localStorage.setItem("token", token)
+    }
+
+    setToken(token)
+  }, [])
+
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+
+  let contextObj = {
+    search,
+    setSearchKey
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider value={contextObj}>
+      <div className="App">
+
+        <header className="App-header">
+          <h1 className="reactify-logo"> Reactify </h1>
+            {!token ?
+              <a
+                href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}
+                className="primaryButton"
+              >
+                Login to Spotify
+              </a>
+              : <button className="primaryButton" onClick={logout}>Logout</button>}
+
+            {token ?
+              <SearchAppBar/>
+              : <p></p>
+            }
+
+            {renderTrack()}
+
+        </header>
+      </div>
+    </AppContext.Provider>
   );
 }
 
